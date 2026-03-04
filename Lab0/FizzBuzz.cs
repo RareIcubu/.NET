80a83ec8880b1c48f9ae8118584a17175a3c65d2
RareIcubu <jjasinski1209@gmail.com>
2026-03-04 17:52:19 +0100

    Added programs

---
+++ Lab0/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0
+{
+    internal class FizzBuzz
+    {
+        int TopNumber = 0;
+        public FizzBuzz(int TopNumber) {
+            this.TopNumber = TopNumber;
+        }
+
+        public void Run() {
+            for (int i = 1; i <= TopNumber; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    Console.WriteLine("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    Console.WriteLine("Fizz");
+                } else if (i % 5 == 0) {
+                    Console.WriteLine("Buzz");
+                } else {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
--- Lab0/Program.cs
+++ Lab0/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Lab0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int TopNumber = int.Parse(Console.ReadLine());
+            FizzBuzz fizzBuzz = new FizzBuzz(TopNumber);
+            fizzBuzz.Run();
+        }
+    }
+}
